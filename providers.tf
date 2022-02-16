cba46ee1ad4dba722a9abd090051f2a4f547b3c2
Brian <brian.james.gomes@gmail.com>
2022-02-16 08:36:06 -0600

    features block added

--- providers.tf
+++ providers.tf
@@ -1,9 +1,12 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 2.65"
+      source = "hashicorp/azurerm"
+      version = "2.96.0"
     }
   }
 }
 
+provider "azurerm" {
+  features {}
+}
